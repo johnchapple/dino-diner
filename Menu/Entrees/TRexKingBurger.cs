80e62d18035aee1d91e43a306096459e96f93283
John Chapple <johnchapple@win.cis.ksu.edu>
2019-09-10 14:28:24 -0500

    next batch

--- Menu/Entrees/TRexKingBurger.cs
+++ Menu/Entrees/TRexKingBurger.cs
@@ -1,10 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
-namespace Menu.Entrees
+namespace DinoDiner.Menu.Entrees
 {
-    class Class1
+    public class TRexKingBurger
     {
+        private bool bun = true;
+        private bool lettuce = true;
+        private bool tomato = true;
+        private bool onion = true;
+        private bool pickle = true;
+        private bool ketchup = true;
+        private bool mustard = true;
+        private bool mayo = true;
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Steakburger Pattie", "Steakburger Pattie", "Steakburger Pattie" };
+                if (bun) ingredients.Add("Whole Wheat Bun");
+                if (lettuce) ingredients.Add("Lettuce");
+                if (tomato) ingredients.Add("Tomato");
+                if (onion) ingredients.Add("Onion");
+                if (pickle) ingredients.Add("Pickle");
+                if (ketchup) ingredients.Add("Ketchup");
+                if (mustard) ingredients.Add("Mustard");
+                if (mayo) ingredients.Add("Mayo");
+                return ingredients;
+            }
+        }
+
+        public TRexKingBurger()
+        {
+            this.Price = 8.45;
+            this.Calories = 728;
+        }
+
+        public void HoldBun()
+        {
+            this.bun = false;
+        }
+
+        public void HoldPeppers()
+        {
+            this.peppers = false;
+        }
+        public void HoldOnion()
+        {
+            this.onion = false;
+        }
     }
 }
