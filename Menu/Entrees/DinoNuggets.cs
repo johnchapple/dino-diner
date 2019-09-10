8da8466a3439bcea9ed30afe4917391c7e6e98c8
John Chapple <johnchapple@win.cis.ksu.edu>
2019-09-10 14:25:05 -0500

    core functionality

---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class DinoNuggets
+    {
+        private uint nuggetCount = 6;
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>();
+                for (int nc = 0; nc > nuggetCount; nc++)
+                {
+                    ingredients.Add("Chicken Nugget");
+                }
+                return ingredients;
+            }
+        }
+
+        public DinoNuggets()
+        {
+            this.Price = 4.25;
+            this.Calories = 59 * this.nuggetCount;
+        }
+
+        public void AddNugget()
+        {
+            this.Price += .59;
+            this.nuggetCount += 1;
+            Ingredients.Add("Chicken Nugget");
+        }
+    }
+}
+
+}
---
+++ Menu/Entrees/PterodactylWings.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class PterodactylWings
+    {
+        private bool bun = true;
+        private bool peppers = true;
+        private bool onions = true;
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Chicken", "Wing Sauce" };
+                return ingredients;
+            }
+        }
+
+        public PterodactylWings()
+        {
+            this.Price = 7.21;
+            this.Calories = 318;
+        }
+
+        public void HoldBun()
+        {
+            this.bun = false;
+        }
+
+        public void HoldPeppers()
+        {
+            this.peppers = false;
+        }
+        public void HoldOnion()
+        {
+            this.onions = false;
+        }
+    }
+}
