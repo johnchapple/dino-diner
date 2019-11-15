3a4b6c358742363e561895e7889f5c609f17c570
John Chapple <johnchapple@win.cis.ksu.edu>
2019-11-15 13:13:39 -0600

    implement basic website

--- Website/Pages/Menu.cshtml.cs
+++ Website/Pages/Menu.cshtml.cs
@@ -4,14 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using DinoDiner.Menu;
 
 namespace Website.Pages
 {
     public class MenuModel : PageModel
     {
+        public Menu menu { get; } = new Menu();
         public void OnGet()
         {
-
         }
     }
 }
